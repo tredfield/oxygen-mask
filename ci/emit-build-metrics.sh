11fbbce9c0ef2944b8f96e01722f7ab0e4bd5568
Travis Redfield <travis.redfield@slalom.com>
2018-04-10 15:32:27 -0700

    fix emit metrics

--- ci/emit-build-metrics.sh
+++ ci/emit-build-metrics.sh
@@ -9,9 +9,37 @@ datadog_api_key=${DATADOG_API_KEY}
 do_merge=${DO_MERGE}
 versions=
 
+getMergeCommit() {
+  pr_number=$(cat ${pull_request_output}/pr_result | jq -r '.number')
+  logInfo "Attempting to get merge commit from pull-request https://github.com/scpprd/${repo}/pull/${pr_number}"
+  merged="false"
+
+  while [ "${merged}" = "false" ]; do
+    pull_request=$(curl -s -H "Authorization: token $GITHUB_ACCESS_TOKEN" "https://api.github.com/repos/scpprd/${repo}/pulls/${pr_number}")
+    merged=$(echo ${pull_request} | jq -r '.merged')
+
+    if [ "${merged}" = "false" ]; then
+      logWarn "Merge commit not found. Sleeping 1 minute"
+      sleep 60
+    else
+      merge_commit_sha=$(echo ${pull_request} | jq -r '.merge_commit_sha')
+      logWarn "Merge commit ${merge_commit_sha} found..."
+      merged_at=$(echo $pull_request | jq -r '.merged_at')
+      merged_at=$(echo "${merged_at}" | sed 's/T/ /' | sed 's/Z//')
+      merged_at=$(date --utc --date="$merged_at" +"%s")
+    fi
+  done
+}
+
+getMergeTime() {
+  create_at=$(cat $pull_request_output/pr_result | jq -r '.merged_at')
+  create_at=$(echo "${create_at}" | sed 's/T/ /' | sed 's/Z//')
+  echo $(date --utc --date="$create_at" +"%s")
+}
+
 wait_time() {
   current_time=$(date +%s)
-  wait_time=$((${current_time}-${merge_start_time}))
+  wait_time=$((${current_time}-${merged_at}))
   echo "$wait_time"
 }
 
@@ -31,20 +59,20 @@ cloneRepoAndMergePrBranchToBase() {
     git push
 
     # save commit`and time
-    merge_start_time=$(date +%s)
-    commit=$(git rev-parse HEAD)
-    echo ${commit} > ${output}/commit_sha
+    merged_at=$(date +%s)
+    merge_commit_sha=$(git rev-parse HEAD)
+    echo ${merge_commit_sha} > ${output}/commit_sha
   fi
 }
 
 getVersions() {
   # get concourse versions and see if has PR
   versions=$(curl -s ${host_name}/api/v1/teams/${team}/pipelines/${build_pipeline}/resources/git-${manifest_repo}/versions)
-  version_id=$(echo $versions | jq -r --arg commit "$commit" '.[] | select(.version.ref == $commit) | .id')
+  version_id=$(echo $versions | jq -r --arg commit "$merge_commit_sha" '.[] | select(.version.ref == $commit) | .id')
 
   if [ -n "$version_id" ]; then
     waiting=$(wait_time)
-    logWarn "Concourse found merge commit ${commit} in (seconds): ${waiting}"
+    logWarn "Concourse found merge commit ${merge_commit_sha} in (seconds): ${waiting}"
     postSeriesMetric "concourse.measure.merge.version.found.duration" ${waiting}
   fi
 }
@@ -61,7 +89,7 @@ pollVersions() {
     # still waiting for version?
     if [ -z "$version_id" ]; then
       waiting=$(wait_time)
-      logWarn "No merge version found. Sleeping ${_sleep} seconds. Total waiting time (seconds): ${waiting}"
+      logWarn "No concourse merge version found. Sleeping ${_sleep} seconds. Total waiting time (seconds): ${waiting}"
       sleep ${_sleep}
     fi
   done
@@ -79,7 +107,7 @@ pollBuildStatus() {
     build_status="pending"
 
     while [[ ("${build_status}" == "pending" || "${build_status}" == "started") ]]; do
-      logInfo "Checking build status for commit ${commit} for pipeline ${build_pipeline} and repo ${manifest_repo}"
+      logInfo "Checking build status for commit ${merge_commit_sha} for pipeline ${build_pipeline} and repo ${manifest_repo}"
       version_input_to=$(curl -s ${host_name}/api/v1/teams/${team}/pipelines/${build_pipeline}/resources/git-${manifest_repo}/versions/${version_id}/input_to)
       statuses_count=$(echo $version_input_to | jq -r '. | length')
 
@@ -113,4 +141,5 @@ pollBuildStatus() {
 
 # main app
 cloneRepoAndMergePrBranchToBase
+getMergeCommit
 pollVersions
